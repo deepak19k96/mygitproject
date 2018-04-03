85ef34e18b9a1467b4b153d3b6adb9bd39cdf65e
Rishi <deepak19k96@gmail.com>
2018-04-03 14:45:00 +0530

    add the category file

---
+++ get_category.php
@@ -0,0 +1,18 @@
+<?php
+$jsonData = json_decode(file_get_contents('https://serviceherd.com/users/servicesproviderformap'));
+$jsonData=json_decode(json_encode($jsonData), True);
+foreach($jsonData as $cate){
+	if($cate['services']){
+	foreach($cate['services'] as $services){
+		if(!empty($services['providers'])){
+		 $cat['name']=$cate['category'];	
+		 $cat['id']=$services['parent'];
+         $procat[]=	$cat;	 
+		}
+	  }
+	}
+}
+echo "<pre>";
+print_R(array_unique($procat));
+echo "</pre>";
+?>
